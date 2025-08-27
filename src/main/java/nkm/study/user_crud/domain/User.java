a5f54f32b12332252fb54fdff65de1a88092481c
NKM <scr08212@gmail.com>
2025-08-27 14:47:11 +0900

    chore: User 엔티티 생성

---
+++ src/main/java/nkm/study/user_crud/domain/User.java
@@ -0,0 +1,24 @@
+package nkm.study.user_crud.domain;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+}
