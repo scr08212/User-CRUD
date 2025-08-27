244d671398681a30e6b666ff1c9ebbc4e2f19190
NKM <scr08212@gmail.com>
2025-08-27 23:31:45 +0900

    feat: User 업데이트 추가

--- src/main/java/nkm/study/user_crud/service/UserService.java
+++ src/main/java/nkm/study/user_crud/service/UserService.java
@@ -15,17 +15,34 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
 
     public User createUser(User user){
-
-        userRepository.findByEmail(user.getEmail())
-            .ifPresent(x->{
-                throw new IllegalStateException("이미 가입된 이메일입니다.");
-            });
+        validateEmail(user.getEmail());
 
         String encodedPassword = passwordEncoder.encode(user.getPassword());
         user.setPassword(encodedPassword);
 
         userRepository.save(user);
         return user;
+    }
+
+    public User updateUser(User user){
+        User currentUser = userRepository.findById(user.getId())
+                .orElseThrow(()->new IllegalStateException("사용자를 찾을 수 없습니다."));
+
+        if(!currentUser.getEmail().equals(user.getEmail())){
+            validateEmail(user.getEmail());
+            currentUser.setEmail(user.getEmail());
+        }
+
+        if(!currentUser.getName().equals(user.getName())){
+            currentUser.setName(user.getName());
+        }
+
+        if(!user.getPassword().isEmpty()){
+            String encodedPassword = passwordEncoder.encode(user.getPassword());
+            currentUser.setPassword(encodedPassword);
+        }
+
+        return userRepository.save(currentUser);
     }
 
     public User deleteUser(User user){
@@ -35,5 +52,12 @@ public class UserService {
 
     public List<User> findAll(){
         return userRepository.findAll();
+    }
+
+    private void validateEmail(String email){
+        userRepository.findByEmail(email)
+                .ifPresent(x->{
+                    throw new IllegalStateException("이미 가입된 이메일입니다.");
+                });
     }
 }
