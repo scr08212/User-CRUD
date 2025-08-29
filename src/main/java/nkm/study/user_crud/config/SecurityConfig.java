07f2ad262129a40fecaeea30e633e8d615d02b61
NKM <scr08212@gmail.com>
2025-08-29 22:58:34 +0900

    feat: SpingConfing filterChain 추가

--- src/main/java/nkm/study/user_crud/config/SecurityConfig.java
+++ src/main/java/nkm/study/user_crud/config/SecurityConfig.java
@@ -3,14 +3,34 @@ package nkm.study.user_crud.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // 임시. 자세한건 조정해야함.
+        http.authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/login").permitAll()
+                .requestMatchers("/").permitAll()
+                .requestMatchers("/admin").hasRole("ADMIN")
+                .requestMatchers("/mypage/**").hasAnyRole("ADMIN", "USER")
+                .anyRequest().authenticated()
+        );
+
+
+        return http.build();
     }
 }
